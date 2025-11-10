aa08438cffad3d9da48ecfc3db05731a91d40241
UrviGangani <202152923+UrviGangani@users.noreply.github.com>
2025-11-10 20:34:03 +0000

    create small counter project - add & Remove value - change the state value via usestate()

--- 01_basicReact/src/App.jsx
+++ 01_basicReact/src/App.jsx
@@ -1,10 +1,27 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  let [counter, setCount] = useState(0);
+  // let counter = 0;
+  const addValue = () => {
+    counter = (counter<20)?counter + 1:counter;
+    console.log(counter)
+    setCount(counter);
+  }
+  const removeValue = () => {
+    counter = (counter > 0)?counter - 1:counter;
+    console.log(counter);
+    setCount(counter);
+  };
   return (
-    <h1>Hello React</h1>
+    <>
+      <h1>Hello React</h1>
+      <h2>Counter value : {counter}</h2>
+      <button onClick={addValue}>Add Value</button>
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+
+  
   )
 }
 
