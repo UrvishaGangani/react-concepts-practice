8e61803a560490a3e00b4722a90c507fd6a607af
UrviGangani <202152923+UrviGangani@users.noreply.github.com>
2025-11-11 16:45:26 +0000

    add Card component with dynamic rendering and Tailwind styling

--- 02_TailwindProps/src/App.jsx
+++ 02_TailwindProps/src/App.jsx
@@ -1,14 +1,39 @@
 import { useState } from 'react'
 import './App.css'
+import Card from './components/Card';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  let allCardDetail = [{
+    img: "https://picsum.photos/400/300?random=20",
+    cardTitle: "Modern Design Solutions",
+    cardDesc: "Discover cutting-edge design patterns and UI components that elevate your digital experiences.",
+    totalviews:234    
+  },
+  {
+    img: "https://picsum.photos/400/300?random=90",
+    cardTitle: "Modern App Solutions",
+    cardDesc: "Discover cutting-edge design patterns and UI components that elevate your digital experiences.",
+    totalviews:1020    
+    },
+  {
+    img: "https://picsum.photos/400/300?random=10",
+    cardTitle: "Modern Web Solutions",
+    cardDesc: "Discover cutting-edge design patterns and UI components that elevate your digital experiences.",
+    totalviews:244    
+  },{
+    img: "https://picsum.photos/400/300?random=15",
+    cardTitle: "Modern Tech Solutions",
+    cardDesc: "Discover cutting-edge design patterns and UI components that elevate your digital experiences.",
+    totalviews:500    
+  }]
   return (
-    <>
-      <div className="min-h-screen flex items-center justify-center bg-red-100">
-        <h1 className="text-blue-600 text-6xl font-bold">Tailwind Works!</h1>
-      </div>
+    <>      
+        <div className="min-h-screen bg-red-100 flex flex-wrap justify-left gap-4 p-4">
+          {allCardDetail.map((card, index) => (
+            // <Card key={index} cardProps={card} />
+            <Card key={index} {...card} />
+          ))}
+        </div>      
     </>
   );
 }
